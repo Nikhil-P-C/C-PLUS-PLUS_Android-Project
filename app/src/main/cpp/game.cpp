766103a087ae7b5e1898684e08750fa4db36ed9d
Nikhil-P-C <nc3030303040@gmail.com>
2025-10-29 11:01:37 +0530

    fixed animation

--- app/src/main/cpp/game.cpp
+++ app/src/main/cpp/game.cpp
@@ -5,65 +5,55 @@
 #include "enet/enet.h"
 #include "input.h"
 #include "SDL3_image/SDL_image.h"
-
+#include "game.h"
 #define LOG_TAG "GAME"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-class Game{
-private:
-    SDL_IOStream* rw = SDL_IOFromFile("sheets/DinoSprites - doux.png", "rb");
-    SDL_Texture* texture = nullptr;
-    static const int SPRITE_HEIGHT =24;
-    static const int SPRITE_WIDTH =24;
-    const float W_scale =10.0f;
-    int windowH =0,windowW=0;
-    bool running =true;
-    SDL_Event event;
-    SDL_Window* window = SDL_CreateWindow("SDL3 Android App", 0, 0, SDL_WINDOW_FULLSCREEN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
-    float x = 100.00f , y =100.00f ;
-public:
-    void run(){
-        int i =1;
-        if(!rw)LOGE("cannot load");
-        else
-            LOGE("loaded");
-        Input input(event);
+void Game::run(){
 
-        SDL_Surface* surface = IMG_Load_IO(rw,1);
-        if(!surface)return;
-        texture = SDL_CreateTextureFromSurface(renderer,surface);
-        if(!texture)return;
-        SDL_DestroySurface(surface);
-        SDL_SetTextureScaleMode(texture,SDL_SCALEMODE_NEAREST);
+    if(!rw)LOGE("cannot load");
+    else
+        LOGE("loaded");
+    Input input(event);
 
-        while(running){
-            if(!window)return;
-            if(!renderer)return;
-            SDL_GetWindowSize(window,&windowW,&windowH);
-            input.eventhandler(running,windowW,windowH,x,y);
-            i =(i+1) % 24;
-            SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255);
-            SDL_RenderClear(renderer);
+    SDL_Surface* surface = IMG_Load_IO(rw,1);
+    if(!surface)return;
+    texture = SDL_CreateTextureFromSurface(renderer,surface);
+    if(!texture)return;
+    SDL_DestroySurface(surface);
+    SDL_SetTextureScaleMode(texture,SDL_SCALEMODE_NEAREST);
+    int current_Frame =0;
+    int frame_delay =50;//delayed by 50ms
+    Uint32 Last_Frame_Time =SDL_GetTicks();
+    while(running){
+        if(!window)return;
+        if(!renderer)return;
+        SDL_GetWindowSize(window,&windowW,&windowH);
+        input.eventhandler(running,windowW,windowH,x,y);
+        //gets player animation indices for sprite rendering
+        input.getAnimationindexes(this->Animation);
+        Uint32 now =SDL_GetTicks();
+        if(now-Last_Frame_Time>frame_delay){
+            if(current_Frame<Animation.lastIndex)current_Frame++;
+            else current_Frame=Animation.startIndex;
+            Last_Frame_Time=now;
 
-            SDL_FRect dst = {x, y, SPRITE_WIDTH*W_scale, SPRITE_HEIGHT*W_scale};
-            SDL_FRect src ={(float)(0+(SPRITE_WIDTH*i)),0,SPRITE_WIDTH,SPRITE_HEIGHT};
-            SDL_RenderTexture(renderer, texture, &src, &dst);
-
-            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-            SDL_FRect Joystick=input.getJoystick();
-            SDL_RenderFillRect(renderer, &Joystick);
-
-            SDL_RenderPresent(renderer);
-            SDL_Delay(16);
-            i++;
         }
-    }
-    ~Game(){
+        //sets color
+        SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255);
+        SDL_RenderClear(renderer);
 
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
+        SDL_FRect dst = {x, y, SPRITE_WIDTH * W_scale, SPRITE_HEIGHT * W_scale};
+        SDL_FRect src = {(float) (0 + (SPRITE_WIDTH * current_Frame)), 0, SPRITE_WIDTH, SPRITE_HEIGHT};
+        SDL_RenderTexture(renderer, texture, &src, &dst);
+
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_FRect Joystick=input.getJoystick();
+        SDL_RenderFillRect(renderer, &Joystick);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
 
     }
-};
+}
---
+++ app/src/main/cpp/game.h
@@ -0,0 +1,35 @@
+//
+// Created by LENOVO on 29-10-2025.
+//
+#include "SDL3/SDL.h"
+#ifndef MY_APP_GAME_H
+#define MY_APP_GAME_H
+struct Animation{
+    int startIndex=0;
+    int lastIndex=0;
+};
+class Game{
+private:
+    Animation Animation;
+    SDL_IOStream* rw = SDL_IOFromFile("sheets/DinoSprites - doux.png", "rb");
+    SDL_Texture* texture = nullptr;
+    static const int SPRITE_HEIGHT =24;
+    static const int SPRITE_WIDTH =24;
+    const float W_scale =10.0f;
+    int windowH =0,windowW=0;
+    bool running =true;
+    SDL_Event event;
+    SDL_Window* window = SDL_CreateWindow("SDL3 Android App", 0, 0, SDL_WINDOW_FULLSCREEN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    float x = 100.00f , y =100.00f ;
+public:
+    void run();
+    ~Game(){
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+
+    }
+};
+
+#endif //MY_APP_GAME_H
